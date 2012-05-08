c3c03dffe979d3fe86e022d0ceede2de9cf29049
Geoff Moller <gmoller@yammer-inc.com>
2012-05-07 22:22:51 -0700

    prototype, constructor local db save mutation

--- public/javascripts/collect/model/link.js
+++ public/javascripts/collect/model/link.js
@@ -3,6 +3,14 @@ collect.Link = function(link){
   data.couchId = link.id;
   data.couchRev = link.value.rev;
   data.tags = data.tags ? link.value.tags.sort() : [];
+  data.tags = _.map(data.tags, function(tag){
+    if(tag === 'prototype' || tag === 'constructor'){
+      return '_' + tag + '_';
+    }
+    else {
+      return tag;
+    }
+  });
   data.dateCreatedDesc = 10000000000000000 - link.value.date_created;
   return data;
 }
